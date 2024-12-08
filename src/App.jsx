07fa3a253f4f8e8778dc2494c5b51abd5426d147
Sachin Doddamani <chriswilderdelta19@gmail.com>
2024-12-08 11:39:05 +0000

    added useRef Hook and able to copy the password now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
         // depends on the length of the password which is 
         // the state var length.
 
-        for (let index = 0; index < length; index++) {
+        for (let i = 0; i < length; i++) {
             const index = Math.floor(Math.random()*(str.length))
             // Dont add +1 for array cases while generating
             // random numbers(produces out of bound, undefined)
@@ -51,15 +51,59 @@ function App() {
   }, 
   [length, numberAllowed, specialCharAllowed, passwordGenerator]
   )
+
+  // Next We want to be able to copy the password, when we click 
+  // on copy button using onClick. make sure to add onclick to
+  // copy btn as well.
+
+  // const clipBoardCopy = useCallback( () =>{
+  //     window.navigator.clipboard.writeText(password);
+  //     // We can write something onto clipboard as done above.
+  // }, [password])
+
+  // Note that what this abve callback does is, whenever copy 
+  // btn is clicked, It will check whether password is changed
+  // from last password it stored. If no it will write old
+  // password again to clipboard. If yes, it will change 
+  // the function defn accordingly. ie, it will set new password.
+
+  // But note that Users wont be able to see whether password
+  // has been copied. For this, whenever click happens we
+  // can fetch reference to passwordField using DOM JS and 
+  // highlight the text. Or we can use another hook
+  // called useRef to this reference.
+
+  const passwordRef = useRef(null)
+
+  // Now we can fetch required html tag anywhere we need, by 
+  // passing this hook var using ref attr of html tag.
+  // Ex : ref= {passwordRef} inside passwordField in our case.
+
+  // But now we must proceed by commenting clipBoardCopy and 
+  // rewriting it again.
+
+  const clipBoardCopy = useCallback( () =>{
+    // Note that we can change DOM of element using
+    // 'current' attr of useRef object.
+    passwordRef.current?.select()
+
+    window.navigator.clipboard.writeText(password);
+    // We can write something onto clipboard as done above.
+  }, [password])
+
+
+
+
   return (
     <div className="App bg-gray-800 w-full h-screen font-mono">
         <h1 className=' text-blue-500 text-3xl font-semibold  '> Password Generator </h1>
         <div className='bg-slate-500 md:w-3/4 md:h-32 h-32 w-full mx-auto flex flex-col justify-center  rounded-md'>
             <div className='flex flex-row justify-center mx-1 mt-3 '>
-              <div className=' bg-slate-100 w-3/4 h-12 pt-3 rounded-md'>
-                  {password}
-              </div>
-              <button className=' bg-sky-500 h-full text-center my-auto text-slate-100 p-3 ml-1 px-3 rounded-md '>
+              <input className='passwordField bg-slate-100 w-3/4 h-12 pl-3 rounded-md'
+                    ref={passwordRef} value={password}>     
+              </input>
+              <button className=' bg-sky-500 h-full text-center my-auto text-slate-100 p-3 ml-1 px-3 rounded-md '
+                  onClick={clipBoardCopy}>
                 Copy
               </button>
             </div>
